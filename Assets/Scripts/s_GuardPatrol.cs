77f8872a5b0b173b07dd888f9e6e7873c51fdb49
ShaunDWhite <111538@student.sae.edu.au>
2015-07-21 15:51:34 +1000

    Guard Sight
    Working on guards line of sight.

--- Assets/Scripts/s_GuardPatrol.cs
+++ Assets/Scripts/s_GuardPatrol.cs
@@ -4,24 +4,32 @@ using System.Collections;
 public class s_GuardPatrol : MonoBehaviour {
 
 	public bool one, two, three, four;
-	public Vector3 Pos1, Pos2, Pos3, Pos4;
 	public float MovingSpeed = 2;
-
+	RaycastHit hit;
+	public float SightDistance = 7.5f;
+	public Transform node1, node2, node3, node4;
+	public Transform target;
 
 	// Use this for initialization
-	void Start () {	
+	void Start () {
+		node1 = GameObject.Find ("Post One").transform;
+		node2 = GameObject.Find ("Post Two").transform;
+		node3 = GameObject.Find ("Post Three").transform;
+		node4 = GameObject.Find ("Post Four").transform;
+		target = node1;
 		one = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Patrol ();
+		Sight ();
+		transform.LookAt (target);
 	}
 
 	void Patrol(){
 		if(one)
 			transform.Translate (5 * MovingSpeed * Time.deltaTime, 0, 0);
-
 
 		if(two)
 			transform.Translate (-(5 * MovingSpeed * Time.deltaTime), 0, 0);
@@ -31,7 +39,17 @@ public class s_GuardPatrol : MonoBehaviour {
 
 		if(four)
 			transform.Translate (0, -(5 * MovingSpeed * Time.deltaTime), 0);
+	}
 
+
+	void Sight(){
+		Vector3 forward = transform.TransformDirection(Vector3.forward) * SightDistance;
+		Debug.DrawRay(transform.position, forward, Color.green);
+
+		Physics.Raycast (transform.position, transform.forward, out hit, SightDistance);
+		if (hit.collider.tag == "Player") {
+			Debug.Log("You been seen");
+				}
 	}
 
 
@@ -43,7 +61,7 @@ public class s_GuardPatrol : MonoBehaviour {
 		}
 		if (col.gameObject.name == "Post Two") {
 			three = true;
-			two = false;
+			two = false;	
 		}
 		if (col.gameObject.name == "Post Three") {
 			one = true;
@@ -51,13 +69,19 @@ public class s_GuardPatrol : MonoBehaviour {
 		}
 		if (col.gameObject.name == "Post Four") {
 			four = true;
-			one = false;
+			one = false;	
 		}
-
-
-
-
 	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.name == "Player") {
+			Debug.Log("you been caught");
+				}
+	}
+
+
+
+
 
 
 
