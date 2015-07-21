9aa2717ba25b2e82cc3576c0777f949b5c7b6d37
ShaunDWhite <111538@student.sae.edu.au>
2015-07-21 16:40:12 +1000

    Guard Sight
    Guard sight has been finished. Doesn't do anything, just Debug.logs "You
    been seen".

--- Assets/Scripts/s_GuardPatrol.cs
+++ Assets/Scripts/s_GuardPatrol.cs
@@ -17,43 +17,48 @@ public class s_GuardPatrol : MonoBehaviour {
 		node3 = GameObject.Find ("Post Three").transform;
 		node4 = GameObject.Find ("Post Four").transform;
 		target = node1;
-		one = true;
+		two = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Patrol ();
 		Sight ();
-		transform.LookAt (target);
+		//transform.LookAt (target);
 	}
 
 	void Patrol(){
 		if(one)
-			transform.Translate (5 * MovingSpeed * Time.deltaTime, 0, 0);
-
+//			transform.Translate (5 * MovingSpeed * Time.deltaTime, 0, 0);
+			transform.Translate(transform.right * 5 * MovingSpeed * Time.deltaTime);
+//
 		if(two)
-			transform.Translate (-(5 * MovingSpeed * Time.deltaTime), 0, 0);
-
+//			transform.Translate (-(5 * MovingSpeed * Time.deltaTime), 0, 0);
+			transform.Translate(-transform.right * 5 * MovingSpeed * Time.deltaTime);
+//
 		if(three)
-			transform.Translate (0, 5 * MovingSpeed * Time.deltaTime, 0);
-
+//			transform.Translate (0, 5 * MovingSpeed * Time.deltaTime, 0);
+			transform.Translate(-transform.up * 5 * MovingSpeed * Time.deltaTime);
+//
 		if(four)
-			transform.Translate (0, -(5 * MovingSpeed * Time.deltaTime), 0);
+//			transform.Translate (0, -(5 * MovingSpeed * Time.deltaTime), 0);
+			transform.Translate(transform.up * 5 * MovingSpeed * Time.deltaTime);
 	}
 
 
 	void Sight(){
-		Vector3 forward = transform.TransformDirection(Vector3.forward) * SightDistance;
+		Vector3 forward = transform.TransformDirection(-Vector3.right) * SightDistance;
 		Debug.DrawRay(transform.position, forward, Color.green);
 
-		Physics.Raycast (transform.position, transform.forward, out hit, SightDistance);
-		if (hit.collider.tag == "Player") {
+		Physics.Raycast (transform.position, -transform.right, out hit, SightDistance);
+		if (hit.collider != null && hit.collider.tag == "Player") {
 			Debug.Log("You been seen");
-				}
+		}
 	}
 
 
 	void OnTriggerEnter(Collider col){
+		transform.Rotate(transform.forward, -90.0f);
 		if (col.gameObject.name == "Post One") {		
 			two = true;
 			four = false;
