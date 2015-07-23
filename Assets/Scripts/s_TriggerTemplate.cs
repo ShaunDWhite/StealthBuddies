83e64037f4c8e035a89a7b1bfef2b70bec1560c1
Filupio <111607@student.sau.edu.au>
2015-07-23 10:04:05 +1000

    Played with Trigger Template
    Added Player as a prefab, we will need to make a player 2 prefab. Added
    Player One and Player Two as tags to use. Internet has been down,
    committing as often as possible.

--- Assets/Scripts/s_TriggerTemplate.cs
+++ Assets/Scripts/s_TriggerTemplate.cs
@@ -6,6 +6,8 @@ public class s_TriggerTemplate : MonoBehaviour {
 
 	public s_GameManager gameManager;
 
+
+
 	// Use this for initialization
 	void Start () {
 		gameManager = GameObject.Find ("GameManager").GetComponent<s_GameManager> ();
@@ -15,4 +17,17 @@ public class s_TriggerTemplate : MonoBehaviour {
 	void Update () {
 	
 	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Player One") {
+			print ("Shiet");
+		} else if (col.tag == "Player Two") {
+			print ("shiet again");
+		}
+	}
+
+
+
+
 }
