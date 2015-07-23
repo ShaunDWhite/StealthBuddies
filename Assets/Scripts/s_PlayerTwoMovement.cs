526bd80d8a74e7ef1daeedf05fff95cd48ebb8b2
ShaunDWhite <111538@student.sae.edu.au>
2015-07-23 15:17:06 +1000

    Basic player 2 movement

---
+++ Assets/Scripts/s_PlayerTwoMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_PlayerTwoMovement : MonoBehaviour {
+
+	public float MovementSpeed = 5f;
+	
+	// Update is called once per frame
+	void Update () {
+		Movement();
+	}
+
+	public void Movement(){
+		if(Input.GetKey(KeyCode.W)){
+			transform.Translate (0, Time.deltaTime * MovementSpeed, 0);
+		}		
+		if(Input.GetKey(KeyCode.A)){
+			transform.Translate (-(Time.deltaTime * MovementSpeed), 0, 0);
+		}		
+		if(Input.GetKey(KeyCode.D)){
+			transform.Translate (Time.deltaTime * MovementSpeed, 0, 0);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			transform.Translate (0, -(Time.deltaTime * MovementSpeed), 0);
+		}
+	}
+}
