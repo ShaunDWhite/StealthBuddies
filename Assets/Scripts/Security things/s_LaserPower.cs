bc32d3f5ab2064d93aebd7d7208f6d02a0915dae
ShaunDWhite <111538@student.sae.edu.au>
2015-07-27 16:11:10 +1000

    Lasers turn off too now

--- Assets/Scripts/Security things/s_LaserPower.cs
+++ Assets/Scripts/Security things/s_LaserPower.cs
@@ -9,6 +9,8 @@ public class s_LaserPower : MonoBehaviour {
 		L_BLOn, L_BLOff,
 		L_BROn, L_BROff;
 
+	public GameObject L1, L2, L3, L4;
+
 	public s_LaserOn L_One, L_Two, L_Three, L_Four;
 
 	// Use this for initialization
@@ -21,6 +23,11 @@ public class s_LaserPower : MonoBehaviour {
 		L_TROff.SetActive(false);
 		L_BLOff.SetActive(false);
 		L_BROff.SetActive(false);
+
+		L1.SetActive(true);
+		L2.SetActive(true);
+		L3.SetActive(true);
+		L4.SetActive(true);
 	}
 	
 	// Update is called once per frame
@@ -35,10 +42,12 @@ public class s_LaserPower : MonoBehaviour {
 		if(L_One.IsTurnedOn){
 			L_TLOn.SetActive(true);
 			L_TLOff.SetActive(false);
+			L1.SetActive(true);
 		}
 		else if(!L_One.IsTurnedOn){
 			L_TLOn.SetActive(false);
 			L_TLOff.SetActive(true);
+			L1.SetActive(false);
 		}
 	}
 
@@ -46,30 +55,36 @@ public class s_LaserPower : MonoBehaviour {
 		if(L_Two.IsTurnedOn){
 			L_TROn.SetActive(true);
 			L_TROff.SetActive(false);
+			L2.SetActive(true);
 		}
 		else if(!L_Two.IsTurnedOn){
 			L_TROn.SetActive(false);
 			L_TROff.SetActive(true);
+			L2.SetActive(false);
 		}
 	}
 	public void ThreeHasPower(){
 		if(L_Three.IsTurnedOn){
 			L_BLOn.SetActive(true);
 			L_BLOff.SetActive(false);
+			L3.SetActive(true);
 		}
 		else if(!L_Three.IsTurnedOn){
 			L_BLOn.SetActive(false);
 			L_BLOff.SetActive(true);
+			L3.SetActive(false);
 		}
 	}
 	public void FourHasPower(){
 		if(L_Four.IsTurnedOn){
 			L_BROn.SetActive(true);
 			L_BROff.SetActive(false);
+			L4.SetActive(true);
 		}
 		else if(!L_Four.IsTurnedOn){
 			L_BROn.SetActive(false);
 			L_BROff.SetActive(true);
+			L4.SetActive(false);
 		}
 	}
 
