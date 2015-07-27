b9d428319f575f37a1e1be8d9734bd5e691544dc
ShaunDWhite <111538@student.sae.edu.au>
2015-07-27 16:40:00 +1000

    Level 2 loads when Both players are at the end of level 1

---
+++ Assets/Scripts/Level Change/s_LoadLevelTwo.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_LoadLevelTwo : MonoBehaviour {
+
+	public bool One = false;
+	public bool Two = false;
+
+
+
+
+
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.gameObject.name == "PlayerOneTest"){
+			One = true;
+		}
+		if(col.gameObject.name == "PlayerTwoTest"){
+			Two = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		if(col.gameObject.name == "PlayerOneTest"){
+			One = false;
+		}
+		if(col.gameObject.name == "PlayerTwoTest"){
+			Two = false;
+		}
+	}
+
+
+	void Update(){
+		LoadNextLevel();		
+	}
+
+	void LoadNextLevel(){
+		if((One == true) && (Two == true)){
+			Application.LoadLevel("Level_2");
+		}
+	}
+
+}
