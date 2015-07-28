d2e0000f4ac220d4966f704b4cbd3406d3f7d62b
ShaunDWhite <111538@student.sae.edu.au>
2015-07-28 13:12:34 +1000

    Started working on the new security on/ off code

--- Assets/Scripts/Security things/s_CameraOff.cs
+++ Assets/Scripts/Security things/s_CameraOff.cs
@@ -6,17 +6,19 @@ public class s_CameraOff : MonoBehaviour {
 
 	public s_CameraOn CameraOne, CameraTwo;
 	public float Timer = 5;
+	public bool P1_Press = false, 
+				P2_Press = false;
 
-	void OnTriggerEnter2D(Collider2D col){
-		if(col.gameObject.name == "PlayerOneTest"){
-			Debug.Log("le button is pressed, timer starts");
-			StartCoroutine(CameraOneOff());
-		}
-		else if(col.gameObject.name == "PlayerTwoTest"){
-			Debug.Log("le Camers 2 button is pressed, timer starts");
-			StartCoroutine(CameraTwoOff());
-		}
-	}
+//	void OnTriggerEnter2D(Collider2D col){
+//		if(col.gameObject.name == "PlayerOneTest"){
+//			Debug.Log("le button is pressed, timer starts");
+//			StartCoroutine(CameraOneOff());
+//		}
+//		else if(col.gameObject.name == "PlayerTwoTest"){
+//			Debug.Log("le Camers 2 button is pressed, timer starts");
+//			StartCoroutine(CameraTwoOff());
+//		}
+//	}
 
 	public IEnumerator CameraOneOff(){
 		CameraOne.IsOn = false;			
@@ -29,4 +31,42 @@ public class s_CameraOff : MonoBehaviour {
 		yield return new WaitForSeconds(Timer);	//waits 5 seconds
 		CameraTwo.IsOn = true;				//camera comes back on 
 	}
+
+
+
+	void Update(){
+//		if (P1_Press) {
+//			if(Input.GetKeyDown(KeyCode.Space)){
+//				StartCoroutine(CameraOneOff());
+//			}
+//		}
+		if (P2_Press) {
+			if(Input.GetKeyDown(KeyCode.Space)){
+				StartCoroutine(CameraTwoOff());
+			}
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.gameObject.name == "PlayerOneTest"){
+			Debug.Log("le button is pressed, timer starts");
+			P1_Press = true;
+		}
+		else if(col.gameObject.name == "PlayerTwoTest"){
+			Debug.Log("le Camers 2 button is pressed, timer starts");
+			P2_Press = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		if(col.gameObject.name == "PlayerOneTest"){
+			P1_Press = false;
+		}
+		else if(col.gameObject.name == "PlayerTwoTest"){
+			P2_Press = false;
+		}
+	}
+
+
+
 }
--- Assets/Scripts/Security things/s_SpotlightOff.cs
+++ Assets/Scripts/Security things/s_SpotlightOff.cs
@@ -5,12 +5,18 @@ public class s_SpotlightOff : MonoBehaviour {
 
 	public s_SpotlightOn LightOne, LightTwo;
 	public float Timer = 5;
+	public bool P2_Press = false;
 
 	void OnTriggerEnter2D(Collider2D col){
 		if(col.gameObject.name == "PlayerTwoTest"){
 			StartCoroutine(LightOff ());
 		}
 	}
+
+
+	void OnTriggerStay2D(Collider2D col){
+
+		}
 
 	public IEnumerator LightOff(){
 		LightOne.LightIsOn = false;
