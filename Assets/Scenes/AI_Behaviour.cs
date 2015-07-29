46ca6e22df886d7252b0ba0d63eda4fed53e11c8
ShaunDWhite <111538@student.sae.edu.au>
2015-07-29 16:47:25 +1000

    Commented out nics codes for now
    Imma test some stuff

--- Assets/Scenes/AI_Behaviour.cs
+++ Assets/Scenes/AI_Behaviour.cs
@@ -104,15 +104,15 @@ public class AI_Behaviour : MonoBehaviour {
        {
            int tempIDX = waypointIDX;
 			//Nic tried to add its waypoint system to reset once it reaches the last point. ***********************************Look Here for Fuck Up***************************************
-           do
-           {
-				if(waypointIDX <= 3){
-               waypointIDX +=1;
-				} else if (waypointIDX > 3)
-				{
-					waypointIDX = 0;
-				}
-           }
+//           do
+//           {
+//				if(waypointIDX <= 3){
+//               waypointIDX +=1;
+//				} else if (waypointIDX > 3)
+//				{
+//					waypointIDX = 0;
+//				}
+//           }
            while (waypointIDX == tempIDX);
 		    currState = AIState.LOOK;
        }
