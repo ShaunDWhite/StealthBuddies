e1ea50098f083dd4b840dcbbf9c77097e443f439
ShaunDWhite <111538@student.sae.edu.au>
2015-07-29 11:58:32 +1000

    Level 3 things
    Security set up
    Starting to code them

---
+++ Assets/Scripts/Security things/s_Level3_Power.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_Level3_Power : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CameraPower();
+		LaserPower();
+		SingleLight();
+		TwinLight();
+	}
+
+
+	void CameraPower(){
+
+	}
+
+	void LaserPower(){
+
+	}
+
+	void SingleLight(){
+
+	}
+
+	void TwinLight(){
+
+	}
+}
