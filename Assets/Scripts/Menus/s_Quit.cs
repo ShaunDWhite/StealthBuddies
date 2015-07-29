c3752f9757b9f4a8b6ab9c85b871960b72a858bc
ShaunDWhite <111538@student.sae.edu.au>
2015-07-29 19:31:32 +1000

    Did some things
    Added a quit script
    Changed the size and speed of guards

---
+++ Assets/Scripts/Menus/s_Quit.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_Quit : MonoBehaviour {
+
+	void Update(){
+		if(Input.GetKey(KeyCode.Escape)){
+			Application.LoadLevel("MainMenu");
+		}
+	}
+}
