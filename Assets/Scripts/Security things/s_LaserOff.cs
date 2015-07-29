f1f51372bae22623163138bf5ccb27b92ddd3495
ShaunDWhite <111538@student.sae.edu.au>
2015-07-29 12:58:28 +1000

    Level 3 things
    Level 3 security stuff works
    Changed laser code

--- Assets/Scripts/Security things/s_LaserOff.cs
+++ Assets/Scripts/Security things/s_LaserOff.cs
@@ -5,26 +5,97 @@ public class s_LaserOff : MonoBehaviour {
 
 	public s_LaserOn LaserOne, LaserTwo, LaserThree, LaserFour;
 	public float timer = 5;
+	public bool L1, L2, L3, L4, LvL3;
+
+
+	void Start(){
+		L1 = false;
+		L2 = false;
+		L3 = false;
+		L4 = false;
+		LvL3 = false;
+	
+	}
+
+
 
 	void OnTriggerEnter2D(Collider2D col){
 		if(col.gameObject.name == "PlayerOneTest"){
-			if(gameObject.name == "LaserOff_TopLeft"){
-				StartCoroutine(LaserThreeOff());
+			if(gameObject.name == "LaserOff_TopLeft"){			
+				L3 = true;
 			}
 			else if(gameObject.name == "LaserOff_TopRight"){
-				StartCoroutine(LaserFourOff());
+				L4 = true;
 			}
 		}
 		else if(col.gameObject.name == "PlayerTwoTest"){
 			if(gameObject.name == "LaserOff_BottomLeft"){
-				StartCoroutine(LaserOneOff());
+				L1 = true;
 			}
 			else if(gameObject.name == "LaserOff_BottomRight"){
-				StartCoroutine(LaserTwoOff());
+				L2 = true;
+			}
+			else {
+				LvL3 = true;
+			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		if(col.gameObject.name == "PlayerOneTest"){
+			if(gameObject.name == "LaserOff_TopLeft"){			
+				L3 = false;
+			}
+			else if(gameObject.name == "LaserOff_TopRight"){
+				L4 = false;
+			}
+		}
+		else if(col.gameObject.name == "PlayerTwoTest"){
+			if(gameObject.name == "LaserOff_BottomLeft"){
+				L1 = false;
+			}
+			else if(gameObject.name == "LaserOff_BottomRight"){
+				L2 = false;
+			}
+			else {
+				LvL3 = false;
+
+			}
+		}
+	}
+
+
+	void Update(){
+		if(L1){
+			if(Input.GetKeyDown(KeyCode.Space)){
+				StartCoroutine(LaserOneOff ());
+			}
+		}
+		if(L2){
+			if(Input.GetKeyDown(KeyCode.Space)){
+				StartCoroutine(LaserTwoOff ());
+			}
+		}
+		if(L3){
+			if(Input.GetButtonDown("A")){
+				StartCoroutine(LaserThreeOff());
+			}
+		}
+		if(L4){
+			if(Input.GetButtonDown("A")){
+				StartCoroutine(LaserFourOff());
+			}
+		}
+		if(LvL3){
+			if(Input.GetKeyDown(KeyCode.Space)){
+				StartCoroutine(LaserOneOff ());
+				StartCoroutine(LaserTwoOff ());
 			}
 		}
 
 	}
+
+
 
 
 	public IEnumerator LaserOneOff(){
--- Assets/Scripts/Security things/s_Level3_Power.cs
+++ Assets/Scripts/Security things/s_Level3_Power.cs
@@ -16,12 +16,20 @@ public class s_Level3_Power : MonoBehaviour {
 		LightThreeCone;
 	public s_CameraOn C_One;
 	public s_SpotlightOn S_One, S_Two;
+	public s_LaserOn L_One;
 
 
 
 	// Use this for initialization
 	void Start () {
 	
+		LaserOne.SetActive(true);
+		LaserTwo.SetActive(true);
+		CameraCone.SetActive(true);
+		LightOneCone.SetActive(true);
+		LightTwoCone.SetActive(true);
+		LightThreeCone.SetActive(true);
+
 	}
 	
 	// Update is called once per frame
@@ -34,18 +42,42 @@ public class s_Level3_Power : MonoBehaviour {
 
 
 	void CameraPower(){
-
+		if(C_One.IsOn){
+			CameraCone.SetActive(true);
+		}
+		else if(!C_One.IsOn){
+			CameraCone.SetActive(false);
+		}
 	}
 
 	void LaserPower(){
-
+		if(L_One.IsTurnedOn){
+			LaserOne.SetActive(true);
+			LaserTwo.SetActive(true);
+		}
+		else if(!L_One.IsTurnedOn){
+			LaserOne.SetActive(false);
+			LaserTwo.SetActive(false);
+		}
 	}
 
 	void SingleLight(){
-
+		if(S_Two.LightIsOn){
+			LightThreeCone.SetActive(true);
+		}
+		else if(!S_Two.LightIsOn){
+			LightThreeCone.SetActive(false);
+		}
 	}
 
 	void TwinLight(){
-
+		if(S_One.LightIsOn){
+			LightOneCone.SetActive(true);
+			LightTwoCone.SetActive(true);
+		}
+		else if(!S_One.LightIsOn){
+			LightOneCone.SetActive(false);
+			LightTwoCone.SetActive(false);
+		}
 	}
 }
