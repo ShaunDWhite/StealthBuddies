6c6f4d8ba1a18d8ee9fc0dca8b9cdb5970da659a
ShaunDWhite <111538@student.sae.edu.au>
2015-08-18 15:19:21 +1000

    Background music works
    It starts in the main menu so you need to start at the main menu for it
    to play

---
+++ Assets/Scripts/s_BGM.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_BGM : MonoBehaviour {
+
+	public static s_BGM instance = null;
+
+		
+	void Awake(){
+
+		if (instance == null) {
+			instance = this;
+		}
+		else if (instance != this) {
+			Destroy (gameObject);    
+		}
+		DontDestroyOnLoad(this.gameObject);
+
+	}
+
+
+	void Start() { 
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
