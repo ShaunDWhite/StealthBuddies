8328843daf8cbfa62759a5520c89cf0e346bad81
ShaunDWhite <111538@student.sae.edu.au>
2015-08-24 14:13:28 +1000

    New end game art is in

--- Assets/Scripts/s_Bars.cs
+++ Assets/Scripts/s_Bars.cs
@@ -3,15 +3,16 @@ using System.Collections;
 
 public class s_Bars : MonoBehaviour {
 
-	public GameObject Bars1, Bars2;
+	public GameObject End_A, End_B, End_C;
 	public s_GameManager gameManager;
 
 
 	// Use this for initialization
 	void Start () {
 		gameManager = GameObject.Find ("GameManager").GetComponent<s_GameManager> ();
-		Bars1.SetActive(false);
-		Bars2.SetActive(false);	
+		End_A.SetActive(false);
+		End_B.SetActive(false);	
+		End_C.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -22,10 +23,13 @@ public class s_Bars : MonoBehaviour {
 
 	public void ShowBars(){
 		if(gameManager.EndOne == true){
-			Bars2.SetActive(true);
+			End_A.SetActive(true);;
 		}
 		else if(gameManager.EndTwo == true){
-			Bars1.SetActive(true);
+			End_B.SetActive(true);		
+		}
+		else if(gameManager.EndBoth == true){
+			End_C.SetActive(true);
 		}
 	}
 }
