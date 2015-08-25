908591d167b26abdf6d354c1342dbd76907758e3
Jhett Black <jhettblack@hotmail.com>
2015-08-26 02:48:30 +1000

    Updated to latest build of CM, implemented versions.

--- Assets/Scripts/Menus/s_Start.cs
+++ Assets/Scripts/Menus/s_Start.cs
@@ -1,12 +1,14 @@
-﻿using UnityEngine;
+using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using System.Collections;
 
-public class s_Start : MonoBehaviour {
+[assembly: System.Reflection.AssemblyVersion("1.0.*")]
+public class s_Start : MonoBehaviour
+{
 
 	public void Yay(){
-		CobaltMetrics.Metrics.StartMetrics ("7bd602166979f7b86a56b08551daa641"); //Metrics
+		CobaltMetrics.Metrics.StartMetrics ("7bd602166979f7b86a56b08551daa641", System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString()); //Metrics
 		Application.LoadLevel("Controlls");
 	}
 
